4476d7d314f90766ce201cd39aa011eafcb62b4a
tuhin47 <tuhintowhidul9@gmail.com>
2017-11-16 13:12:01 +0600

    css found

--- app.js
+++ app.js
@@ -30,15 +30,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 // app.use('/users', users);
 app.use('/subjects',subjects);
@@ -50,6 +41,18 @@ app.use('/upload',upload);
 app.use('/addUser',addUser);
 app.use('/adduser',addUser);
 app.use('/students',students);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/students',express.static(path.join(__dirname, 'public')));
 
 
 
