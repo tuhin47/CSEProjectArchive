a17b5dbabccd44cb52cc4c6221749bdf0e953f75
tuhin <tuhintowhidul9@gmail.com>
2017-11-24 15:26:26 +0600

    fix some issue

--- routes/students.js
+++ routes/students.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var students = require('../controllers/studentscontroller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('students');
+  res.redirect('/batches');
 });
 router.get('/:reg', students.data);
 
