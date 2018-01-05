8dcf8136cb21e7e8b848e4d2e72d007eb93735b3
alif al amin <alifalamin4@gmail.com>
2018-01-05 17:58:32 +0600

    abc

--- controllers/teachers_controllers.js
+++ controllers/teachers_controllers.js
@@ -9,7 +9,7 @@ exports.teacherprofile = function(req, res) {
   console.log("in teacherprofile");
   res.render('teacher');
 };
-
+ 
 exports.addteacherprofile = function(req, res) {
   console.log("in add teacherprofile");
   res.render('addteacher');
