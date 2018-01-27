0965b3dc178c9dfc83879a46370923e8b3900871
tuhin <tuhintowhidul9@gmail.com>
2018-01-27 22:19:30 +0600

    done all links

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Projects = require('../models/project');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('subjects');
+  Projects.find({}, function(err, results) {
+    if (err) throw err;
+    console.log(results);
+    res.render('subjects', {
+      results: results
+    });
+  });
 });
 
 module.exports = router;
