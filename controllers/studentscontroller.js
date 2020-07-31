d4adab30f7820ab3cffa05024e6929e738dcff78
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-07-31 11:19:36 +0600

    fine tune

--- controllers/studentscontroller.js
+++ controllers/studentscontroller.js
@@ -1,5 +1,20 @@
 var Users = require('../models/user');
 
+exports.allStudents = function(req,res,next){
+  Users.find({},
+  function(err, results) {
+    if (err) throw err;
+    if (results.toString() === '') {
+      res.redirect('/');
+    }
+    
+    res.render('allstudents', {
+      results:results,
+      page:"students"
+    });
+  });
+}
+
 
 exports.data = function(req, res, next) {
   
--- routes/students.js
+++ routes/students.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 var students = require('../controllers/studentscontroller');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/batches');
-});
+router.get('/', students.allStudents);
 router.get('/:reg', students.data);
 
 router.get('/update/:reg',students.updatedata);
